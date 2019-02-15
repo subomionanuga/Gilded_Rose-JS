e5280f9ac76187db0091d2aee7ba40ca74947e10
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-15 17:31:56 +0000

    test: first passing test for Aged brie

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,21 +1,21 @@
-var {Shop, Item} = require('../src/gilded_rose.js');
+var {Shop, Item} = require('../src/gilded_rose.js')
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
-    var gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    var items = gildedRose.updateQuality();
-    expect(items[0].name).toEqual("foo");
-  });
-});
+    var gildedRose = new Shop([ new Item("foo", 0, 0) ])
+    var items = gildedRose.updateQuality()
+    expect(items[0].name).toEqual("foo")
+  })
+})
 
 describe("Normal Items", function() {
-  var items;
-  var gildedRose;
+  var items
+  var gildedRose
 
   beforeEach(function() {
-    gildedRose = new Shop([ new Item("My goodies", 10, 50) ]);
-    items = gildedRose.updateQuality();
-  });
+    gildedRose = new Shop([ new Item("My goodies", 10, 50) ])
+    items = gildedRose.updateQuality()
+  })
 
   it("Checks that the name of the item is stored", function() {
     expect(items[0].name).toEqual("My goodies")
@@ -27,5 +27,28 @@ describe("Normal Items", function() {
 
   it("Quality reduces by 1 each day", function() {
     expect(items[0].quality).toEqual(49)
+  })
+
+  it("checks that quality reduces by 2 if sellIn 0", function() {
+    gildedRose = new Shop([ new Item("My goodies", 0, 48) ])
+    items = gildedRose.updateQuality()
+    expect(items[0].quality).toEqual(46)
+  })
+
+})
+
+describe("Aged Brie", function() {
+  var items
+  var gildedRose
+
+  // beforeEach(function() {
+  //   gildedRose = new Shop([ new Item("Aged Brie", 10, 50) ])
+  //   items = gildedRose.updateQuality()
+  // })
+
+  it("quality of Aged Bried increases by 1", function() {
+    gildedRose = new Shop([ new Item("Aged Brie", 15, 30) ])
+    items = gildedRose.updateQuality()
+    expect(items[0].quality).toEqual(31)
   })
 })

2a4e23ec10b5fdffd0813175f55856d5c54054e7
Subomi Onanuga <subomi_onanuga@yahoo.co.uk>
2019-02-15 16:39:06 +0000

    test: test added and passing for checking normal items sell by date

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -2,9 +2,22 @@ var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
   it("should foo", function() {
-    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
-    const items = gildedRose.updateQuality();
+    var gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    var items = gildedRose.updateQuality();
     expect(items[0].name).toEqual("foo");
   });
-
 });
+
+describe("Normal Items", function() {
+  var items;
+  var gildedRose;
+
+  beforeEach(function() {
+    gildedRose = new Shop([ new Item("My goodies", 10, 50) ]);
+    items = gildedRose.updateQuality();
+  });
+
+  it("Sell by date reduces by 1 each day", function() {
+    expect(items[0].sellIn).toEqual(9)
+  })
+})
